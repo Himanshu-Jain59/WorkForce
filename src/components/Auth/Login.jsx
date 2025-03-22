9fe6814a4aaca47730026d7f69df29289c5525ab
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-22 21:23:33 +0530

    submitHandler

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(`email:${email} and pwd: ${password}`);
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen ">
       <div className="flex border-4 w-120 h-110 border-emerald-500 rounded-2xl items-center justify-center">
-        <form className="flex  flex-wrap flex-col gap-6 w-full mx-10">
+        <form
+          onSubmit={submitHandler}
+          className="flex  flex-wrap flex-col gap-6 w-full mx-10"
+        >
           <h1 className="text-2xl font-bold">Welcome</h1>
           <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             className="text-lg border-2 rounded-2xl px-6 py-4 focus:outline-none"
             type="email"
-            name=""
-            id=""
             placeholder="Enter your email"
           />
           <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             className="text-lg border-2 rounded-2xl px-6 py-4 focus:outline-none"
             type="password"
-            name=""
-            id=""
             placeholder="Enter your password"
           />
           <button className="text-2xl  rounded-full px-6 py-3 bg-emerald-600 cursor-pointer">
