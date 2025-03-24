c8d58ad1eccc89f28a61223b6c1fc8b8147f6524
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-24 22:47:53 +0530

    admin dashboard

---
+++ src/components/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import CreateTask from "./CreateTask";
+function AdminDashboard() {
+  return (
+    <div>
+      <Header />
+      <CreateTask />
+    </div>
+  );
+}
+
+export default AdminDashboard;
