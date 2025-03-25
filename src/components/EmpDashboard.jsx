c4b24fabb744c93f5471b7bacc6dad4497c2870e
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-25 23:14:18 +0530

    data prop

--- src/components/EmpDashboard.jsx
+++ src/components/EmpDashboard.jsx
@@ -6,19 +6,19 @@ import Completetask from "./Taskcount/Completetask";
 import Failedtask from "./Taskcount/Failedtask";
 import Tasklist from "./Tasklist/Tasklist";
 
-function EmpDashboard() {
+function EmpDashboard({ data }) {
   return (
     <div className="w-screen bg-[#1c1c1c] h-screen">
-      <Header />
+      <Header data={data} />
       <div className="mx-10 flex justify-between gap-5">
-        <Newtask />
-        <Accepttask />
-        <Completetask />
-        <Failedtask />
+        <Newtask data={data} />
+        <Accepttask data={data} />
+        <Completetask data={data} />
+        <Failedtask data={data} />
       </div>
       <div id="tasklist" className=" m-10 overflow-x-scroll h-70 gap-5 flex">
-        <Tasklist />
-        <Tasklist />
+        <Tasklist data={data} />
+        <Tasklist data={data} />
         <Tasklist />
         <Tasklist />
         <Tasklist />
