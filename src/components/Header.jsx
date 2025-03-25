20c33ea9c55adfe0faefcb59f168f0fd24677f7b
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-25 23:14:33 +0530

    data prop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Header() {
+function Header({ data }) {
+  console.log(data);
   return (
     <div className="flex  justify-between  p-10">
       <h1 className="text-2xl font-semibold">
         Hello, <br />
-        <span className="text-3xl">Employee 👋</span>
+        <span className="text-3xl">{data.firstName} 👋</span>
       </h1>
       <button className="mt-3 text-xl font-semibold bg-red-600 h-fit p-3 rounded-md cursor-pointer">
         Log out
